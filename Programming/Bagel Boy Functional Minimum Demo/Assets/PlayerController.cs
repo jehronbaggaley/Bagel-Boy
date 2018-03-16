a306ebb325aa6caf73af39635c2fce4a030ad866
Eiredryll <TankP@Teancum-Prices-MacBook.local>
2018-03-15 20:36:52 -0600

    Movement
    Camera and Basic movement are done! Finally!

---
+++ Programming/Bagel Boy Functional Minimum Demo/Assets/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+	public float jumpForce;
+	public CharacterController controller;
+
+	private Vector3 moveDirection;
+	public float gravityScale;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float yStore = moveDirection.y;
+		moveDirection = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
+		moveDirection = moveDirection.normalized * moveSpeed;
+		moveDirection.y = yStore;
+
+		if (controller.isGrounded) {
+			moveDirection.y = 0f;
+			if (Input.GetButtonDown("Jump")) {
+				moveDirection.y = jumpForce;
+			}
+		}
+
+		moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
+		controller.Move (moveDirection * Time.deltaTime);
+	}
+}

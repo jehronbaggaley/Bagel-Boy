9d5e1892a3b272ad5f0c4475732f73cf75ba521f
Eiredryll <33163500+Eiredryll@users.noreply.github.com>
2018-03-23 13:00:46 -0600

    Pause menu
    pause menu is getting close to being implemented

---
+++ Programming/LeonNguyen/Game Menu Unity/Assets/Game/Pause Menu/PauseMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+	
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Programming/LeonNguyen/PauseMenu/Assets/PauseMenu.cs
+++ Programming/LeonNguyen/PauseMenu/Assets/PauseMenu.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PauseMenu : MonoBehaviour {
 
 	
-	}
+
 	
 	// Update is called once per frame
 	void Update () {

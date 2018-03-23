2f2463b5dba5c91179e0f3bf2e0134cf62466811
Eiredryll <33163500+Eiredryll@users.noreply.github.com>
2018-03-23 12:45:09 -0600

    Merged the main menu and demo
    The main menu and the demo are in the same project now, hopefully the pause menu is next!

---
+++ Programming/LeonNguyen/Game Menu Unity/Assets/Game/CameraControl.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+	public Transform target;
+
+	public Vector3 offset;
+
+	public bool useOffsetValues;
+
+	public float rotateSpeed;
+
+	public Transform pivot;
+
+	public float maxViewAngle;
+	public float minViewAngle;
+
+	// Use this for initialization
+	void Start () {
+		if (!useOffsetValues) {
+			offset = target.position - transform.position;
+		}
+
+		pivot.transform.position = target.transform.position;
+		pivot.transform.parent = target.transform;
+
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+		float horizontal = Input.GetAxis ("Mouse X") * rotateSpeed;
+		target.Rotate (0, horizontal, 0);
+
+		float vertical = Input.GetAxis ("Mouse Y") * rotateSpeed;
+		pivot.Rotate (-vertical, 0, 0);
+
+		if (pivot.rotation.eulerAngles.x > maxViewAngle && pivot.rotation.eulerAngles.x < 180f) {
+			pivot.rotation = Quaternion.Euler (maxViewAngle, 0, 0);
+		}
+		if (pivot.rotation.eulerAngles.x > 180f && pivot.rotation.eulerAngles.x < minViewAngle) {
+			pivot.rotation = Quaternion.Euler (minViewAngle, 0, 0);
+		}
+
+		float desiredYAngle = target.eulerAngles.y;
+		float desiredXAngle = pivot.eulerAngles.x;
+
+		Quaternion rotation = Quaternion.Euler (desiredXAngle, desiredYAngle, 0);
+		transform.position = target.position - (rotation * offset);
+
+		if (transform.position.y < (target.position.y - 0.5f)) {
+			transform.position = new Vector3 (transform.position.x, target.position.y - 0.5f, transform.position.z);
+		}
+
+		transform.LookAt (target);
+	}
+}
---
+++ Programming/LeonNguyen/Game Menu Unity/Assets/Game/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+	public float jumpForce;
+	public CharacterController controller;
+
+	private Vector3 moveDirection;
+	public float gravityScale;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController> ();
+
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float yStore = moveDirection.y;
+		moveDirection.y = 0;
+		moveDirection = (transform.forward * Input.GetAxisRaw("Vertical")) + (transform.right * Input.GetAxisRaw("Horizontal"));
+		moveDirection = moveDirection.normalized * moveSpeed;
+		moveDirection.y = yStore;
+
+		if (controller.isGrounded) {
+			moveDirection.y = 0f;
+			if (Input.GetButtonDown("Jump")) {
+				moveDirection.y = jumpForce;
+			}
+		}
+
+		moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
+		controller.Move (moveDirection * Time.deltaTime);
+	}
+}

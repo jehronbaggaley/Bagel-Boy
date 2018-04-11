b4e77d277d4649726921e1f81fb91032967e15df
Eiredryll <33163500+Eiredryll@users.noreply.github.com>
2018-04-11 12:51:38 -0600

    Visual feedback

--- Programming/LeonNguyen/Game Menu Unity/Assets/Game/HealthManager.cs
+++ Programming/LeonNguyen/Game Menu Unity/Assets/Game/HealthManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthManager : MonoBehaviour {
 
@@ -12,17 +13,35 @@ public class HealthManager : MonoBehaviour {
 	public float InvincibilityLength;
 	private float InvincibilityCounter;
 
+	public Renderer playerRenderer;
+	private float flashCounter;
+	public float flashLength = 0.1f;
+
+	public Text playerHealth;
+
 	// Use this for initialization
 	void Start () {
 		currHealth = maxHealth;
 
 		thePlayer = FindObjectOfType<PlayerController>();
+
+		playerHealth.text = "" + currHealth + "/" +maxHealth;
 	}
 
 	void Update () {
 		if (InvincibilityCounter > 0) {
 			InvincibilityCounter -= Time.deltaTime;
+
+			flashCounter -= Time.deltaTime;
+			if (flashCounter <= 0) {
+				playerRenderer.enabled = !playerRenderer.enabled;
+			}
+
+			if (InvincibilityCounter <= 0) {
+				playerRenderer.enabled = true;
+			}
 		}
+		playerHealth.text = "" + currHealth + "/" +maxHealth;
 	}
 
 	public void HurtPlayer(int damage, Vector3 direction) {
@@ -32,6 +51,9 @@ public class HealthManager : MonoBehaviour {
 			thePlayer.Knockback(direction);
 
 			InvincibilityCounter = InvincibilityLength;
+
+			playerRenderer.enabled = false;
+			flashCounter = flashLength;
 		}
 	}
 	public void HealPlayer(int healing) {

636f51c12ba5d049e90cfbece392f856ebe00222
Leon Nguyen <33161653+TheGreatKingLeon@users.noreply.github.com>
2018-05-01 11:44:13 -0600

    new

---
+++ Programming/LeonNguyen/The Game/Assets/Game/AudioPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour {
+
+	public AudioClip hurtSound;
+	public AudioClip deathSound;
+
+	public AudioSource soundSource;
+
+	public void Hurt() {
+		soundSource.clip = hurtSound;
+		soundSource.Play();
+	}
+	public void Death() {
+		soundSource.clip = deathSound;
+		soundSource.Play();
+	}
+}
---
+++ Programming/LeonNguyen/The Game/Assets/Game/CheckPoint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour {
+
+	public HealthManager theHealthMan;
+
+	// Use this for initialization
+	void Start () {
+		theHealthMan = FindObjectOfType<HealthManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		Debug.Log("Hit");
+		if (other.gameObject.name == "PlayerController") {
+			Debug.Log("Player");
+			theHealthMan.SetSpawnPoint (transform.position);
+		}
+	}
+}
